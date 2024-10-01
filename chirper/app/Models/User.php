8977767c4ceb034539e9defdc5afec839a36c270
Ross Mason <N00230645@iadt.ie>
2024-10-01 09:31:51 +0100

    completed creating chirps

--- chirper/app/Models/User.php
+++ chirper/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -16,6 +17,9 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+
+     
     protected $fillable = [
         'name',
         'email',
@@ -44,4 +48,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
+
+    
 }
